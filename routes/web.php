49403b1422c6e66d09563af1465399a66c45c79e
Farhandhika Nurrohman <farhandhikanurrohman@gmail.com>
2026-09-18 14:45:46 +0700

    Update 5 Membuat form log history asset, menambahkan icon untuk penghapusan aset, dan menambahkan fitur download pdf history asset

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,17 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () { 
     return redirect('/login'); 
 });
+
+Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'authenticate'])->middleware('guest');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::middleware('auth')->group(function () {
     Route::get('/assets/dashboard', [AssetController::class, 'dashboard'])->name('assets.dashboard');
     Route::get('/assets/export', [AssetController::class, 'exportExcel'])->name('assets.export');
-
-    Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
-    Route::post('/login', [AuthController::class, 'authenticate']);
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    
     Route::get('/assets-history', [AssetController::class, 'history'])->name('assets.history');
     Route::get('/assets-history/pdf', [AssetController::class, 'exportHistoryPDF'])->name('assets.history.pdf');
-
+    
     Route::resource('assets', AssetController::class);
 });
